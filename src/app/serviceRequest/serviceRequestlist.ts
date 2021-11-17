c4986550b1ff3145bf173615c63523dbc3e69865
Ayush Aher <ayushaher118@gmail.com>
2021-11-17 10:20:44 +0530

    Added Service Request No. for all types of users in the main grid

--- src/app/serviceRequest/serviceRequestlist.ts
+++ src/app/serviceRequest/serviceRequestlist.ts
@@ -1,16 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
-import { User, Customer, Country, DistributorRegion, ProfileReadOnly, ServiceRequest } from '../_models';
-import { Router, ActivatedRoute } from '@angular/router';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { first } from 'rxjs/operators';
-import { ColDef,GridApi,ColumnApi} from 'ag-grid-community'; 
-import { environment } from '../../environments/environment';
+import {Country, ProfileReadOnly, ServiceRequest, User} from '../_models';
+import {ActivatedRoute, Router} from '@angular/router';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {first} from 'rxjs/operators';
+import {ColDef, ColumnApi, GridApi} from 'ag-grid-community';
+import {environment} from '../../environments/environment';
 import {
-  AccountService, AlertService, CustomerService, CountryService, NotificationService,
-  ProfileService, ServiceRequestService, ContactService
+  AccountService,
+  AlertService,
+  ContactService,
+  CountryService,
+  CustomerService,
+  NotificationService,
+  ProfileService,
+  ServiceRequestService
 } from '../_services';
-import { RenderComponent } from '../distributor/rendercomponent';
+import {RenderComponent} from '../distributor/rendercomponent';
 
 
 @Component({
@@ -39,6 +45,7 @@ export class ServiceRequestListComponent implements OnInit {
   IsDistributorView: boolean = false;
   IsEngineerView: boolean = false;
   distId: any;
+
   constructor(
     private formBuilder: FormBuilder,
     private route: ActivatedRoute,
@@ -52,11 +59,10 @@ export class ServiceRequestListComponent implements OnInit {
     private serviceRequestService: ServiceRequestService,
     private contcactservice: ContactService
   ) {
-    
+
   }
-  
+
   ngOnInit() {
-    //debugger;
     this.user = this.accountService.userValue;
     this.profilePermission = this.profileService.userProfileValue;
     if (this.profilePermission != null) {
@@ -75,13 +81,11 @@ export class ServiceRequestListComponent implements OnInit {
       this.IsCustomerView = true;
       this.IsDistributorView = false;
       this.IsEngineerView = false;
-    }
-    else if (this.user.roleId == environment.distRoleId) {
+    } else if (this.user.roleId == environment.distRoleId) {
       this.IsCustomerView = false;
       this.IsDistributorView = true;
       this.IsEngineerView = false;
-    }
-    else {
+    } else {
       this.IsCustomerView = false;
       this.IsDistributorView = false;
       this.IsEngineerView = true;
@@ -90,9 +94,10 @@ export class ServiceRequestListComponent implements OnInit {
       .pipe(first())
       .subscribe({
         next: (data: any) => {
+          console.log(data)
           this.distId = data.object.defdistid;
           this.getallrecored();
-       },
+        },
         error: error => {
           //  this.alertService.error(error);
           this.notificationService.showSuccess(error, "Error");
@@ -101,22 +106,20 @@ export class ServiceRequestListComponent implements OnInit {
       });
 
     // this.distributorId = this.route.snapshot.paramMap.get('id');
-   
+
     if (this.IsDistributorView) {
       this.columnDefs = this.createDisColumnDefs();
-    }
-    else if (this.IsEngineerView) {
+    } else if (this.IsEngineerView) {
       this.columnDefs = this.createDisColumnDefs();
-    }
-    else {
+    } else {
       this.columnDefs = this.createCustColumnDefs();
     }
   }
 
   Add() {
-    this.router.navigate(['servicerequest']);  
+    this.router.navigate(['servicerequest']);
   }
- 
+
 
   private createDisColumnDefs() {
     return [{
@@ -133,20 +136,30 @@ export class ServiceRequestListComponent implements OnInit {
         deleteLink: 'SR',
         deleteaccess: this.hasDeleteAccess
       },
-    },{
-      headerName: 'Customer Name',
-      field: 'companyname',
-      filter: true,
-      enableSorting: true,
-      editable: false,
-        sortable: true,
-      tooltipField: 'companyname',
-    }, {
-      headerName: 'Site Name',
-      field: 'sitename',
-      filter: true,
+    },
+      {
+        headerName: 'Service Request No.',
+        field: 'serreqno',
+        filter: true,
+        enableSorting: true,
         editable: false,
-      sortable: true
+        sortable: true,
+        tooltipField: 'Service Request No.',
+      },
+      {
+        headerName: 'Customer Name',
+        field: 'companyname',
+        filter: true,
+        enableSorting: true,
+        editable: false,
+        sortable: true,
+        tooltipField: 'companyname',
+      }, {
+        headerName: 'Site Name',
+        field: 'sitename',
+        filter: true,
+        editable: false,
+        sortable: true
       },
       {
         headerName: 'Machine Serial No.',
@@ -194,41 +207,53 @@ export class ServiceRequestListComponent implements OnInit {
         deleteLink: 'SR',
         deleteaccess: this.hasDeleteAccess
       },
-    }, {
-      headerName: 'Distributor Name',
-      field: 'distributor',
-      filter: true,
-      enableSorting: true,
-      editable: false,
-      sortable: true,
-      tooltipField: 'distributor',
-    }, {
-      headerName: 'Site Name',
-      field: 'sitename',
-      filter: true,
-      editable: false,
-      sortable: true
     },
-    {
-      headerName: 'Machine Serial No.',
-      field: 'machineModelName',
-      filter: true,
-      editable: false,
-      sortable: true
-    },
-    {
-      headerName: 'Machine Model Name',
-      field: 'machmodelname',
-      filter: true,
-      editable: false,
-      sortable: true
-    },
-    {
-      headerName: 'Contact Person',
-      field: 'contactperson',
-      filter: true,
-      editable: false,
-      sortable: true
+      {
+        headerName: 'Service Request No.',
+        field: 'serreqno',
+        filter: true,
+        enableSorting: true,
+        editable: false,
+        sortable: true,
+        tooltipField: 'Service Request No.',
+      },
+      {
+        headerName: 'Distributor Name',
+        field: 'distributor',
+        filter: true,
+        enableSorting: true,
+        editable: false,
+        sortable: true,
+        tooltipField: 'distributor',
+      },
+      {
+        headerName: 'Site Name',
+        field: 'sitename',
+        filter: true,
+        editable: false,
+        sortable: true
+      },
+
+      {
+        headerName: 'Machine Serial No.',
+        field: 'machineModelName',
+        filter: true,
+        editable: false,
+        sortable: true
+      },
+      {
+        headerName: 'Machine Model Name',
+        field: 'machmodelname',
+        filter: true,
+        editable: false,
+        sortable: true
+      },
+      {
+        headerName: 'Contact Person',
+        field: 'contactperson',
+        filter: true,
+        editable: false,
+        sortable: true
       },
       {
         headerName: 'Country',
@@ -241,11 +266,9 @@ export class ServiceRequestListComponent implements OnInit {
   }
 
 
-  
   onGridReady(params): void {
     this.api = params.api;
     this.columnApi = params.columnApi;
-    this.api.sizeColumnsToFit();
   }
 
   getallrecored() {
@@ -253,6 +276,7 @@ export class ServiceRequestListComponent implements OnInit {
       .pipe(first())
       .subscribe({
         next: (data: any) => {
+          console.log(data)
           this.srCustList = data.object.filter(x => x.createdby == this.user.userId);
           this.srDistList = data.object.filter(x => x.distid == this.distId);
           this.srEngList = data.object.filter(x => x.assignedto == this.user.contactId);
