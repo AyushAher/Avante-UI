6bb8110c42e31ef824ea4469b8ebeb69165b5abf
Ayush Aher <ayushaher118@gmail.com>
2021-11-21 10:08:04 +0530

    accidentaly reseted head

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -18,6 +18,7 @@ export const environment = {
   CRMA: "eeacf156-1d31-11ec-92d1-fc45964f576b",
   zohocodeapi: "https://accounts.zoho.com/oauth/v2/auth?scope=ZohoBooks.fullaccess.all&client_id=1000.LC5XN00TMI5IFL8R9T7FZYFTEOMLKY&response_type=code&redirect_uri=https://localhost:44378/custpayrpt&access_type=offline",
   zohoaccessapi:"https://accounts.zoho.com/oauth/v2/token?code={0}&client_id=1000.LC5XN00TMI5IFL8R9T7FZYFTEOMLKY&client_secret=c3bda561c4b6ccb5a9064c828a6f892389e8c4f121&redirect_uri=https://localhost:44378/custpayrpt&grant_type=authorization_code",
+
   client: "1000.UI60D3EU4DEZJZOS0G873SEAUPNY0A",
   secret: "4192fa27f500e7a8a0b23f960d2d78d26de84c56c3",
   redirecturl: "https://localhost:44378/custpayrpt",
