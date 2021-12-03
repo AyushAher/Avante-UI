b810b7e162624461684109beb5748d6eaa013a8e
Ayush Aher <ayushaher118@gmail.com>
2021-12-04 03:44:57 +0530

    Spare Parts Recommended Permissions Fixed

--- src/app/sparepartsrecommended/sparepartsrecommended.component.ts
+++ src/app/sparepartsrecommended/sparepartsrecommended.component.ts
@@ -47,7 +47,7 @@ export class SparepartsrecommendedComponent implements OnInit {
     this.profilePermission = this.profileService.userProfileValue;
     if (this.profilePermission != null) {
       let profilePermission = this.profilePermission.permissions.filter(
-        (x) => x.screenCode == "SCURR"
+        (x) => x.screenCode == "SPRCM"
       );
       if (profilePermission.length > 0) {
         this.hasReadAccess = profilePermission[0].read;
