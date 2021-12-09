c8743b3a49b70afd6a6fc587688232083f2073d4
Kishore Gund <kishoregund@hotmail.com>
2021-12-09 12:43:43 +0530

    Zohoapi - config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,9 +3,10 @@ export const environment = {
   apiUrl: 'https://service.avantgardeinc.com/api/api',
   uiUrl: 'https://service.avantgardeinc.com/',
 
-  custRoleId: "36eb70f8-0247-11ec-8e31-fc45964f576b",
-  distRoleId: "36fafd78-0247-11ec-8e31-fc45964f576b",
-  engRoleId: "374154ac-0247-11ec-8e31-fc45964f576b",
+  custRoleCode: "RCUST",
+  distRoleCode: "RDTSP",
+  engRoleCode: "RENG",
+
 
   INS: "INS",
   ANAS: "ANAS",
@@ -14,11 +15,14 @@ export const environment = {
   REWK:"REWK",
   CRMA: "CRMA",
 
-  zohocodeapi: "https://accounts.zoho.com/oauth/v2/auth?scope=ZohoBooks.fullaccess.all&client_id=1000.LC5XN00TMI5IFL8R9T7FZYFTEOMLKY&response_type=code&redirect_uri=https://localhost:44378/custpayrpt&access_type=offline",
-  zohoaccessapi:"https://accounts.zoho.com/oauth/v2/token?code={0}&client_id=1000.LC5XN00TMI5IFL8R9T7FZYFTEOMLKY&client_secret=c3bda561c4b6ccb5a9064c828a6f892389e8c4f121&redirect_uri=https://localhost:44378/custpayrpt&grant_type=authorization_code",
+  zohocodeapi: "https://accounts.zoho.com/oauth/v2/auth?scope=ZohoBooks.fullaccess.all&client_id=1000.LC5XN00TMI5IFL8R9T7FZYFTEOMLKY&response_type=code&redirect_uri=https://service.avantgardeinc.com/custpayrpt&access_type=offline",
+  zohoaccessapi:"https://accounts.zoho.com/oauth/v2/token?code={0}&client_id=1000.LC5XN00TMI5IFL8R9T7FZYFTEOMLKY&client_secret=c3bda561c4b6ccb5a9064c828a6f892389e8c4f121&redirect_uri=https://service.avantgardeinc.com/custpayrpt&grant_type=authorization_code",
 
-  // client: "1000.UI60D3EU4DEZJZOS0G873SEAUPNY0A",
-  // secret: "4192fa27f500e7a8a0b23f960d2d78d26de84c56c3",
-  // redirecturl: "https://localhost:44378/custpayrpt",
-  bookapi:"https://books.zoho.com/api/v3"
+  client: "1000.B3E6SJ92Q73ECGWMH78XIANG9CA2XB",
+  secret: "fe6678494763975fc429bccfc9361a3d8ca53407ef",
+  redirecturl: "https://service.avantgardeinc.com/custpayrpt",
+  bookapi: "https://books.zoho.com/api/v3",
+
+
+
 };
