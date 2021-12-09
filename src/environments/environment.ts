f58a06f507a07dc5d8bbf5cd33d85016e182356f
Ayush Aher <ayushaher118@gmail.com>
2021-12-09 18:25:27 +0530

    Environment variables intgrated

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,38 +3,62 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  //prod:
-  // apiUrl: 'https://service.avantgardeinc.com/api/api',
-  // uiUrl: 'https://service.avantgardeinc.com/',
-  // dev :
-  apiUrl: 'https://localhost:44317/api',
-  uiUrl: 'https://localhost:44378/',
-  configTypeCode: "CONTY",// "f0bf6d8d-739a-11eb-adfc-0a91af0598e6" //"4c155b3e-7526-11eb-97d1-1c39472d435b"
+  //for dev:
+
+  // production: false,
+  // apiUrl: 'https://localhost:44317/api',
+  // uiUrl: 'https://localhost:44378/',
+  // configTypeCode: "CONTY",// "f0bf6d8d-739a-11eb-adfc-0a91af0598e6" //"4c155b3e-7526-11eb-97d1-1c39472d435b"
+  //
+  // custRoleCode: "RCUST",
+  // distRoleCode: "RDTSP",
+  // engRoleCode: "RENG",
+  //
+  // INS: "INS",
+  // ANAS: "ANAS",
+  // PRMN1: "PRMN1",
+  // PRMN2: "PRMN2",
+  // REWK:"REWK",
+  // CRMA: "CRMA",
+  //
+  // zohocodeapi: "https://accounts.zoho.com/oauth/v2/auth?scope=ZohoBooks.fullaccess.all&client_id=1000.LC5XN00TMI5IFL8R9T7FZYFTEOMLKY&response_type=code&redirect_uri=http://localhost:4200/custpayrpt&access_type=offline",
+  // zohoaccessapi:"https://accounts.zoho.com/oauth/v2/token?code={0}&client_id=1000.LC5XN00TMI5IFL8R9T7FZYFTEOMLKY&client_secret=c3bda561c4b6ccb5a9064c828a6f892389e8c4f121&redirect_uri=http://localhost:4200/custpayrpt&grant_type=authorization_code",
+  //
+  // client: "1000.UI60D3EU4DEZJZOS0G873SEAUPNY0A",
+  // secret: "4192fa27f500e7a8a0b23f960d2d78d26de84c56c3",
+  // redirecturl: "http://localhost:4200/custpayrpt",
+  // bookapi: "https://books.zoho.com/api/v3",
+  //
+  // // client: "1000.UI60D3EU4DEZJZOS0G873SEAUPNY0A",
+  // // secret: "4192fa27f500e7a8a0b23f960d2d78d26de84c56c3",
+  // // redirecturl: "https://localhost:44378/custpayrpt",
+
+  // for prod
+
+  production: true,
+  apiUrl: 'https://service.avantgardeinc.com/api/api',
+  uiUrl: 'https://service.avantgardeinc.com/',
 
   custRoleCode: "RCUST",
   distRoleCode: "RDTSP",
   engRoleCode: "RENG",
+  configTypeCode: "CONTY",// "f0bf6d8d-739a-11eb-adfc-0a91af0598e6" //"4c155b3e-7526-11eb-97d1-1c39472d435b"
+
 
   INS: "INS",
   ANAS: "ANAS",
   PRMN1: "PRMN1",
   PRMN2: "PRMN2",
-  REWK:"REWK",
+  REWK: "REWK",
   CRMA: "CRMA",
 
-  zohocodeapi: "https://accounts.zoho.com/oauth/v2/auth?scope=ZohoBooks.fullaccess.all&client_id=1000.LC5XN00TMI5IFL8R9T7FZYFTEOMLKY&response_type=code&redirect_uri=http://localhost:4200/custpayrpt&access_type=offline",
-  zohoaccessapi:"https://accounts.zoho.com/oauth/v2/token?code={0}&client_id=1000.LC5XN00TMI5IFL8R9T7FZYFTEOMLKY&client_secret=c3bda561c4b6ccb5a9064c828a6f892389e8c4f121&redirect_uri=http://localhost:4200/custpayrpt&grant_type=authorization_code",
+  zohocodeapi: "https://accounts.zoho.com/oauth/v2/auth?scope=ZohoBooks.fullaccess.all&client_id=1000.B3E6SJ92Q73ECGWMH78XIANG9CA2XB&response_type=code&redirect_uri=https://service.avantgardeinc.com/custpayrpt&access_type=offline",
+  zohoaccessapi: "https://accounts.zoho.com/oauth/v2/token?code={0}&client_id=1000.B3E6SJ92Q73ECGWMH78XIANG9CA2XB&client_secret=fe6678494763975fc429bccfc9361a3d8ca53407ef&redirect_uri=https://service.avantgardeinc.com/custpayrpt&grant_type=authorization_code",
 
-  client: "1000.UI60D3EU4DEZJZOS0G873SEAUPNY0A",
-  secret: "4192fa27f500e7a8a0b23f960d2d78d26de84c56c3",
-  redirecturl: "http://localhost:4200/custpayrpt",
+  client: "1000.B3E6SJ92Q73ECGWMH78XIANG9CA2XB",
+  secret: "fe6678494763975fc429bccfc9361a3d8ca53407ef",
+  redirecturl: "https://service.avantgardeinc.com/custpayrpt",
   bookapi: "https://books.zoho.com/api/v3",
-
-  // client: "1000.UI60D3EU4DEZJZOS0G873SEAUPNY0A",
-  // secret: "4192fa27f500e7a8a0b23f960d2d78d26de84c56c3",
-  // redirecturl: "https://localhost:44378/custpayrpt",
-
 };
 
 /*
