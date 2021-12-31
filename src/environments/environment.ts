d8657f9bae2925e0bf2cda8e26e4025a6dde4d78
Ayush Aher <ayushaher118@gmail.com>
2021-12-31 18:04:08 +0530

    Zoho Code updated

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -21,8 +21,8 @@ export const environment = {
   // REWK:"REWK",
   // CRMA: "CRMA",
   //
-  // zohocodeapi: "https://accounts.zoho.com/oauth/v2/auth?scope=ZohoBooks.fullaccess.all&client_id=1000.LC5XN00TMI5IFL8R9T7FZYFTEOMLKY&response_type=code&redirect_uri=http://localhost:4200/custpayrpt&access_type=offline",
-  // zohoaccessapi:"https://accounts.zoho.com/oauth/v2/token?code={0}&client_id=1000.LC5XN00TMI5IFL8R9T7FZYFTEOMLKY&client_secret=c3bda561c4b6ccb5a9064c828a6f892389e8c4f121&redirect_uri=http://localhost:4200/custpayrpt&grant_type=authorization_code",
+  // zohocodeapi: "https://accounts.zoho.com/oauth/v2/auth?scope=ZohoBooks.fullaccess.all&client_id=1000.5H07NQJOLXW69IEHWG3GICTVU8L51W&response_type=code&redirect_uri=http://localhost:4200/custpayrpt&access_type=offline",
+  // zohoaccessapi:"https://accounts.zoho.com/oauth/v2/token?code={0}&client_id=1000.5H07NQJOLXW69IEHWG3GICTVU8L51W&client_secret=2f54ec5f719c6ee911a367c16211f8d3576378d013&redirect_uri=http://localhost:4200/custpayrpt&grant_type=authorization_code",
   //
   // client: "1000.UI60D3EU4DEZJZOS0G873SEAUPNY0A",
   // secret: "4192fa27f500e7a8a0b23f960d2d78d26de84c56c3",
