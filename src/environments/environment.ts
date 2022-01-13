3a990ad4867801d614ed7b761f13c425337cfefe
Kishore Gund <kishoregund@hotmail.com>
2022-01-13 15:42:28 +0530

    modified

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -43,7 +43,7 @@ export const environment = {
 
   // zohocodeapi: "https://accounts.zoho.com/oauth/v2/auth?scope=ZohoBooks.fullaccess.all&client_id=1000.0JAHNX3N4V33CK2BTWDZV374B0NMKY&response_type=code&redirect_uri=https://service.avantgardeinc.com/custpayrpt&access_type=offline",
   // zohoaccessapi: "https://accounts.zoho.com/oauth/v2/token?code={0}&client_id=1000.B3E6SJ92Q73ECGWMH78XIANG9CA2XB&client_secret=fe6678494763975fc429bccfc9361a3d8ca53407ef&redirect_uri=https://service.avantgardeinc.com/custpayrpt&grant_type=authorization_code",
-  zohocodeapi: "https://accounts.zoho.com/oauth/v2/auth?scope=ZohoBooks.fullaccess.all&client_id=1000.0JAHNX3N4V33CK2BTWDZV374B0NMKY&response_type=code&redirect_uri=https://service.avantgardeinc.com/custpayrpt&access_type=offline&prompt=consent",
+  zohocodeapi: "https://accounts.zoho.com/oauth/v2/auth?scope=ZohoBooks.fullaccess.all&client_id=1000.5H07NQJOLXW69IEHWG3GICTVU8L51W&response_type=code&redirect_uri=http://localhost:4200/custpayrpt&access_type=offline",
   // client: "1000.B3E6SJ92Q73ECGWMH78XIANG9CA2XB",
   // secret: "fe6678494763975fc429bccfc9361a3d8ca53407ef",
   // redirecturl: "https://service.avantgardeinc.com/custpayrpt",
