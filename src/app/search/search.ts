8f9ff9a7903ea8ebb53f820a2daa76727e8d70c1
AyushAher <ayushaher118@gmail.com>
2022-05-08 03:48:46 +0530

    filter only for distributor

--- src/app/search/search.ts
+++ src/app/search/search.ts
@@ -8,6 +8,7 @@ import { ColDef, ColumnApi, GridApi } from 'ag-grid-community';
 
 import { AccountService, AlertService, CustomerSiteService, InstrumentService, NotificationService } from '../_services';
 import { lnkRenderComponent } from './lnkrendercomponent';
+import { environment } from 'src/environments/environment';
 
 
 @Component({
@@ -16,18 +17,114 @@ import { lnkRenderComponent } from './lnkrendercomponent';
 })
 export class SearchComponent implements OnInit {
 
+  user: User;
+  form: FormGroup;
+  instrumentList: Instrument[];
+  customersite: CustomerSite[];
+  loading = false;
+  submitted = false;
+  isSave = false;
+  customerSiteId: string;
+  customerId: string;
+  type: string = "D";
+  searchKeyword: search;
+  visible: boolean = false;
+
   public columnDefs: ColDef[];
   private columnApi: ColumnApi;
   private api: GridApi;
 
   showGrid: any;
-  instrumentList: any;
 
-  constructor() { }
+  @ViewChild('sform') testFormElement;
+  isDist: boolean;
+  constructor(
+    private fb: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+    private accountService: AccountService,
+    private alertService: AlertService,
+    private instrumentService: InstrumentService,
+    private customerSiteService: CustomerSiteService,
+    private notificationService: NotificationService,
+  ) {
 
-  ngOnInit() {
-    this.columnDefs = this.createColumnDefs();
   }
+  ngOnInit() {
+
+    let role = JSON.parse(localStorage.getItem('roles'));
+    this.user = this.accountService.userValue;
+    this.customerSiteService.getAllCustomerSites()
+      .pipe(first())
+      .subscribe({
+        next: (data: any) => {
+          //debugger;
+          this.customersite = data.object;
+        },
+        error: error => {
+
+          this.loading = false;
+        }
+      });
+
+    if (this.user.username != "admin") {
+      role = role[0]?.itemCode;
+      if (role == environment.distRoleCode) this.isDist = true;
+      else {
+        this.toggleFilter();
+      }
+    }
+
+    //debugger;
+    this.form = this.fb.group({
+      search: [''],
+      custSiteId: [''],
+      isactive: [true],
+
+    });
+    this.columnDefs = this.createColumnDefs();
+    if (JSON.parse(localStorage.getItem('search')) != null) {
+      this.form.get('search').setValue(JSON.parse(localStorage.getItem('search')).search);
+      this.form.get('custSiteId').setValue(JSON.parse(localStorage.getItem('search')).custSiteId);
+      this.searchKeyword = new search;
+      this.searchKeyword.search = JSON.parse(localStorage.getItem('search')).search;
+      this.searchKeyword.custSiteId = JSON.parse(localStorage.getItem('search')).custSiteId;
+      //localStorage.removeItem('search');
+
+      //  this.testFormElement.ngSubmit.emit();
+    }
+  }
+
+  ngAfterViewInit() {
+    if (this.searchKeyword.search != null || this.searchKeyword.custSiteId != null) {
+      this.instrumentService.searchByKeyword(this.searchKeyword.search, this.searchKeyword.custSiteId)
+        .pipe(first())
+        .subscribe({
+          next: (data: any) => {
+            //debugger;
+            this.visible = true;
+            this.instrumentList = data.object;
+            // localStorage.setItem('search', JSON.stringify(this.searchKeyword));
+          },
+          error: error => {
+            //this.alertService.error(error);
+
+            this.loading = false;
+          }
+        });
+    }
+  }
+
+  public testMethod(): void {
+    this.testFormElement.nativeElement.submit();
+  }
+  get f() { return this.form.controls; }
+
+  Add() {
+    this.router.navigate(['customersite', this.customerId]);
+  }
+
+
 
 
   DataFilter(event) {
@@ -82,6 +179,32 @@ export class SearchComponent implements OnInit {
     this.columnApi = params.columnApi;
     this.api.sizeColumnsToFit();
   }
+
+  onSubmit() {
+    //debugger;
+    if (this.form.invalid) {
+      return;
+    }
+    this.searchKeyword = this.form.value
+
+    //this.customerId = this.route.snapshot.paramMap.get('id');
+    this.instrumentService.searchByKeyword(this.searchKeyword.search, this.searchKeyword.custSiteId)
+      .pipe(first())
+      .subscribe({
+        next: (data: any) => {
+          //debugger;
+          this.visible = true;
+          this.instrumentList = data.object;
+          localStorage.setItem('search', JSON.stringify(this.searchKeyword));
+        },
+        error: error => {
+          //this.alertService.error(error);
+
+          this.loading = false;
+        }
+      });
+  }
+
 }
 
 export class search {
